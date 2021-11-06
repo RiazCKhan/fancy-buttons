754b7f98b5b11d72822f7313d74a44548ea4fb56
RiazK <RiazCKhan@gmail.com>
2021-11-06 15:15:47 -0400

    Import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Fancy Buttons!</h1>
+      <AngryButton />
+      <CounterButton />
+      <LightSwitchButton />
+      <TextRepeaterButton />
     </div>
   );
 }
