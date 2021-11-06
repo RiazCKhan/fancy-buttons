2602734b809165a7a6354d63f68c87fcd20fa306
RiazK <RiazCKhan@gmail.com>
2021-11-06 15:13:05 -0400

    Add button components

---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,9 @@
+const CounterButton = function (){
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+}
+
+export default CounterButton;
---
+++ src/components/TextRepeaterButton.jsx
@@ -0,0 +1,9 @@
+const TextRepeaterButton = function (){
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
